9142acf1ca7cf35d9a745f33af1754186147a38b
py2076 <98632955+py2076@users.noreply.github.com>
2022-02-22 17:38:34 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -76,6 +76,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv1 = clientSocket.recv(1024)
     #print(recv1)
     # Fill in end
+    clientSocket.close()
 
 
 if __name__ == '__main__':
